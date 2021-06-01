6c8af7b1892a7b5fe4fd0251755f437a1c23b666
Nischal Rajitha <nischalmadikiri10@gmail.com>
2021-06-01 23:13:15 +0530

    Create SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+package com.company;
+
+  public class SumOfNumbers{
+    public static void main(String[] args){
+      int num1 = 10, num2 = 12, num3 = 6, sum;
+      sum = num1 + num2 + num3;
+      System.out.println("The sum of the three numbers is: " + sum);
+    }
+  }
