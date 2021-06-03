d227e570d24d9050d977f9e5a3fc20e320a10c1f
Nischal Rajitha <nischalmadikiri10@gmail.com>
2021-06-03 14:27:24 +0530

    Update and rename SumOfNumbers.java to CWH_02_SumOfNumbers.java

--- CWH_02_SumOfNumbers.java
+++ CWH_02_SumOfNumbers.java
@@ -1,6 +1,6 @@
 package com.company;
 
-  public class SumOfNumbers{
+  public class CWH_02_SumOfNumbers{
     public static void main(String[] args){
       int num1 = 10, num2 = 12, num3 = 6, sum;
       sum = num1 + num2 + num3;
